a3b6f6d7c868d8908a430a266f4cc256f2003302
carlos676767 <carlitobr730@gmail.com>
2024-03-30 10:49:05 -0700

    funcao separada

--- mulher/src/main.ts
+++ mulher/src/main.ts
@@ -1,15 +1,19 @@
-const mulher = document.querySelector(".mulher") as HTMLImageElement
+
  
-
-
 const manipularQuery = () => {
+  const mulher = document.querySelector(".mulher") as HTMLImageElement
+  const imagemquery = document.querySelector(".imagemquery") as HTMLDivElement
   if (matchMedia("(max-width: 768px)").matches) {
     mulher.src = "src/base-apparel-coming-soon-master/images/hero-mobile.jpg"
-    mulher.style.width = "100%"
-    mulher.style.height = "100%"
-    
+    setarLarguraEtamanho(mulher)
+    imagemquery.appendChild(mulher)
   }
 }
 
+
+const setarLarguraEtamanho = (imagem: HTMLImageElement) => {
+  imagem.style.width = "100%"
+  imagem.style.height = "100%"
+}
 
 manipularQuery()
