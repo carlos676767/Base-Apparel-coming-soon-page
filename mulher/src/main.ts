56dbc97bb4076a8c61c931e1e9d64441d8b20da2
carlos676767 <carlitobr730@gmail.com>
2024-03-30 10:44:49 -0700

    imagem ok

--- mulher/src/main.ts
+++ mulher/src/main.ts
@@ -1,1 +1,15 @@
+const mulher = document.querySelector(".mulher") as HTMLImageElement
+ 
 
+
+const manipularQuery = () => {
+  if (matchMedia("(max-width: 768px)").matches) {
+    mulher.src = "src/base-apparel-coming-soon-master/images/hero-mobile.jpg"
+    mulher.style.width = "100%"
+    mulher.style.height = "100%"
+    
+  }
+}
+
+
+manipularQuery()
